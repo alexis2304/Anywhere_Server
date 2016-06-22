2df9ee153337f45bbc6ed6d03b3b9773b7fdbe9d
alexis2304 <lostrom2011@gmail.com>
2016-06-22 13:23:01 +0400

    Update

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ var ip = require('ip');
  
 var Myip = ip.address();
 
-var ServerVersion = '0.65b Stable';
+var ServerVersion = '0.66b Stable';
 var ServerName    = 'Medusa';
 var ServerCreator = 'Alexis Gougaut'
 
